1a2fcfaa4ff21107e459c17277f7c7367cfaccb9
BR7T <buratinivictor@gmail.com>
2024-03-01 22:58:52 -0300

    need to fix start button

--- script.js
+++ script.js
@@ -15,36 +15,50 @@ const ButtonStart = document.getElementById('buttonStart')
 const removebg = BotPickVisor.style.backgroundImage = 'none';
 const NamePick = document.getElementById('NamePick')
 let CanClick = true;
-
+let NamePlayer = document.getElementById('nomePlayerInput')
+  const Nome = document.getElementById('nomePlayer')
 let ActualScore = 0;
 let HighScore = 0;
+
+
+
+
+
+
+
 
 ActualScoreText.innerHTML = ActualScore;
 HighScoreText.innerHTML = HighScore;
 
 randomN = Math.floor(Math.random() * 3);
 
-let finalizaFunc = 0;
+let finalizaFunc = 'Name';
 
 let Apresentation = document.createElement("div");
 
+
+
 function GameStart() {
- ButtonStart.removeAttribute('onclick')
+  
+  ButtonStart.removeAttribute('onclick')
+  turnOnLights()
   switch(finalizaFunc){
     case 0:
-      AppearText("Rock, Paper, Scissors", Apresentation);
-      finalizaFunc = 1
-      CanClick=false
-
       
-
+      finalizaFunc = 'Name'
+      CanClick=false
+      AppearText("Rock, Paper, Scissors", Apresentation);
     break;
     case 1:
+      
+      
+      
       CanClick = false
-      finalizaFunc = 2
       AppearText(' Level - ' + (ActualScore+1), Apresentation);
+      finalizaFunc = 2
     break;
     case 2:
+
       CanClick = true;
 
       Start();
@@ -59,9 +73,17 @@ function GameStart() {
       AppearText('You Lose!' , Apresentation)
       finalizaFunc = 1;
       break;
-  }
-}
+      case 'Name':
+        finalizaFunc = 1
+ 
 
+        NameArea()
+        
+        
+      break;
+  }
+  
+}
 function AppearText(t, div) {
   ScreenDiv.appendChild(div);
   CanClick = false
@@ -84,9 +106,12 @@ let PickPlay = 'pedra'
 
 
 function Start(){
+
+
+  document.getElementById('VS').innerHTML = '<hr/>VS<hr/>'
+
   UserPickVisor.style.animation='alternate both infinite pulsePick .8s steps(5)'
   BotPickVisor.style.animation='alternate both infinite pulsePick .8s steps(5)'
-
 
   document.getElementById('NumbScore').style.display = 'flex'
     QM.style.display = 'block'
@@ -124,11 +149,10 @@ scissorsPick.addEventListener('click',()=>{
 
 })
 
-selectBtn.addEventListener('click',()=>{
-  NamePick.innerHTML = ''
+selectBtn.addEventListener('click',function selectFunction(){
   
   if(!CanClick){
-    return
+    return 
   }
   CanClick=false
 
@@ -155,6 +179,7 @@ selectBtn.addEventListener('click',()=>{
   }
   
 })
+
 
 
 
@@ -327,7 +352,12 @@ function Draw(){
 
   ActualScore = ActualScore
     ActualScoreText.innerHTML = ActualScore;
-  document.getElementById('VS').innerHTML = 'DRAW'
+  document.getElementById('VS').innerHTML = '<hr/>DRAW<hr/>'
+  setTimeout(()=>{
+  document.getElementById('VS').innerHTML = '<hr/>Play Again<hr/>'
+
+  },500)
+
 
 }
 
@@ -336,4 +366,33 @@ function VerificaPlacar(){
   if(ActualScore > HighScore){
     HighScoreText.innerHTML = ActualScore
   }
+}
+
+function turnOnLights(){
+  ScreenDiv.style.backgroundColor = '#9B9E4D' 
+  document.getElementById('score').style.backgroundColor = "#9B9E4D"
+  document.getElementById('onlight').style.backgroundColor = 'greenyellow'
+  document.getElementById('onlight').style.boxShadow ='0 0 15px 8px greenyellow'
+}
+
+function NameArea(){
+  
+  document.getElementById('nameP').style.display = 'flex'
+  document.getElementById('nomePlayerInput').focus()
+ 
+  ButtonStart.addEventListener('click' , ()=>{
+    if(NamePlayer.value.length === 3 ){
+      document.getElementById('nameP').style.display = 'none'
+      Nome.innerText = NamePlayer.value
+      
+
+      GameStart()
+    }else if(NamePlayer.value.length == 0){
+      textName.innerHTML = 'Você precisa ter um nome!'
+    }else if(NamePlayer.value.length >0 && NamePlayer.value.length<3){
+      textName.innerHTML = 'Seu nome precisa ter 3 letras!'
+
+    }
+  })
+  
 }
